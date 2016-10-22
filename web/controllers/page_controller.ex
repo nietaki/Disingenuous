a637b3e09aa0617075bf8bc0d105be5193e33179
Jacek Królikowski <nietaki@gmail.com>
2016-10-22 17:56:50 +0100

    very careful branding

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule Disingenuous.PageController do
   use Disingenuous.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    render conn, "index.html", page_title: "Home page"
   end
 end
