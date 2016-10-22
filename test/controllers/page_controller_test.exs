3d0652f6c2a20cdb89645622ba08f12863662dde
Jacek Królikowski <nietaki@gmail.com>
2016-10-22 19:17:51 +0100

    bunny dragging example copied from pixi.js

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule Disingenuous.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Navigation"
+    assert html_response(conn, 200) =~ "Phoenix docs"
   end
 end
--- web/static/js/app.js
+++ web/static/js/app.js
@@ -19,3 +19,6 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+import "./dragging"
+
+console.log("app.js")
