3844021cdd9b47a43ef41083d6bacac83b1777ab
Jacek Królikowski <nietaki@gmail.com>
2016-11-01 23:51:28 +0000

    lowered my expectations to have some js unit tests

---
+++ test/js/board.js
@@ -0,0 +1,11 @@
+import assert from 'assert';
+import * as board from '../../web/static/js/board';
+
+
+describe('board', function() {
+  describe('test', function() {
+    it('equals test', function () {
+      assert.equal(board.test, "test");
+    });
+  });
+});
