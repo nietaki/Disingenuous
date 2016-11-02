aa756f22b43ca636f847bceebb73f7a9166b6f3b
Jacek Królikowski <nietaki@gmail.com>
2016-11-02 00:52:42 +0000

    started building out the board logic

--- test/js/board.js
+++ test/js/board.js
@@ -1,11 +1,57 @@
 import assert from 'assert';
 import * as board from '../../web/static/js/board';
 
-
 describe('board', function() {
   describe('test', function() {
     it('equals test', function () {
       assert.equal(board.test, "test");
     });
   });
+
+  describe('directions', function() {
+    it('contains the expected directions', function() {
+      assert(board.directions.has(0))
+      assert(board.directions.has(5))
+      assert(!board.directions.has(6))
+    });
+  });
 });
+
+function p(x, y) {
+  return new board.Point(x, y);
+}
+
+describe('Point', function() {
+  let fours;
+  before(function() {
+    fours = new board.Point(4,4);
+  })
+
+  describe('constructor', function() {
+    it('works', function() {
+      assert.equal(fours.x, 4)
+      assert.equal(fours.y, 4)
+    })
+  })
+
+  describe('equals', function() {
+    it('works on same items', function() {
+      assert(fours.equals(fours));
+      assert(fours.equals(new board.Point(4, 4)));
+      assert.deepEqual(fours, new board.Point(4, 4));
+    })
+    it('works on different items', function() {
+      assert(!fours.equals(new board.Point(4, 5)));
+      assert(!fours.equals(new board.Point(5, 4)));
+    })
+  })
+
+  describe('moved()', function() {
+    it('moves left', function() {
+      assert(p(5, 4).equals(fours.moved(0)))
+    });
+    it('moves LD', function() {
+      assert(p(5, 5).equals(fours.moved(1)))
+    });
+  });
+})
--- web/static/js/board.js
+++ web/static/js/board.js
@@ -1,1 +1,33 @@
-export let test = "test" 
+export let test = "test"
+import assert from 'assert'
+
+
+export const directions = new Set([0, 1, 2, 3, 4, 5])
+Object.freeze(directions)
+
+export class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  equals(otherPoint) {
+    assert(typeof(otherPoint.x) != "undefined")
+    assert(typeof(otherPoint.y) != "undefined")
+
+    return (this.x == otherPoint.x) && (this.y == otherPoint.y)
+  }
+
+  moved(direction) {
+    switch (direction) {
+      case 0:
+        return new Point(this.x + 1, this.y)
+      default:
+        assert(direction >= 0 && direction < 6)
+    }
+  }
+
+  toString() {
+    return `(${this.x}, ${this.y})`;
+  }
+}
